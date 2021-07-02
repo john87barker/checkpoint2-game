3d5e507da09c7ae262e8c959c2dd7adb847cd344
john87barker <john87barker@gmail.com>
2021-07-02 11:49:38 -0600

    cheese works for reals now.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 let cheese = 0
 
 function getCheese() {
-  for (let i = 0; i <= 999999999999999999999999999999999999999999999; i++) {
-    cheese += 1
-    console.log("Cheese collected")
-    return 'cheese +1'
-  }
+
+  cheese += 1
   update()
 }
 
+
+
+
+
+
+
+
+
+
+
 function update() {
-  document.getElementById('cheese').innerText = 'cheese'
+  document.getElementById('cheese').innerText = cheese
 }
+
+update()
